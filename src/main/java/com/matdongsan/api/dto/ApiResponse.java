5f832e6faf4477b2eda17861cc79ef6d60384f0c
ian Kwon <sunghwan.ian.kwon@gmail.com>
2025-04-29 22:26:55 +0900

    feat: 공통 Response 추가

---
+++ src/main/java/com/matdongsan/api/dto/ApiResponse.java
@@ -0,0 +1,30 @@
+package com.matdongsan.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ApiResponse<T> {
+  private int status;
+  private String message;
+  private T data;
+
+  private ApiResponse(int status, String message, T data) {
+    this.status = status;
+    this.message = message;
+    this.data = data;
+  }
+
+  public static <T> ApiResponse<T> success(T data) {
+    return new ApiResponse<>(200, "Success", data);
+  }
+
+  public static <T> ApiResponse<T> fail(String message) {
+    return new ApiResponse<>(400, message, null);
+  }
+}
+
