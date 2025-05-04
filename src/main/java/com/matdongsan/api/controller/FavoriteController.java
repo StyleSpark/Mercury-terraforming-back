2f3ab412079ad62d3b5b659363d87241402dabe4
ian Kwon <sunghwan.ian.kwon@gmail.com>
2025-05-04 20:23:08 +0900

    chore: favorite 컨트롤러 커뮤니티 관련 주석 추가

--- src/main/java/com/matdongsan/api/controller/FavoriteController.java
+++ src/main/java/com/matdongsan/api/controller/FavoriteController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/favorites")
 public class FavoriteController {
-  // 매물 찜 등록
-
-  // 매물 찜 해제
+  // 매물 찜 등록 & 해제
 
   // 찜 목록 조회
+
+  // 커뮤니티 즐겨찾기 등록 & 해제
+
+  // 커뮤니티 즐겨찾기 조회
+
 }
