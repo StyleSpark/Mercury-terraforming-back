2d89504670266a8539c8f083206adcafc074f0ba
ian Kwon <sunghwan.ian.kwon@gmail.com>
2025-05-17 19:46:06 +0900

    chore: 메소드 설명 주석 추가 및 환경 변수 추가

--- src/main/java/com/matdongsan/api/util/JwtUtil.java
+++ src/main/java/com/matdongsan/api/util/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
@@ -11,8 +12,16 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-  private final String secret = "b9F7r4LZtYkMv1xWcS8dU2QoEjNaP6Rh"; // 환경 변수로 분리 해야함
+  @Value("${security.google.secret-key}")
+  private String secret;
 
+  /**
+   * access token 및 유저 정보 발행 메소드
+   * @param userId
+   * @param email
+   * @param role
+   * @return
+   */
   public String generateToken(Long userId, String email, String role) {
     Date now = new Date();
     Date expiry = new Date(now.getTime() + 1000 * 60 * 60 * 24); // 1일
@@ -27,6 +36,11 @@ public class JwtUtil {
             .compact();
   }
 
+  /**
+   * 로그인 한 유저의 정보를 가져오기 위한 parse 메소드
+   * @param token
+   * @return
+   */
   public Claims parseToken(String token) {
     return Jwts.parserBuilder()
             .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8)))
